abb39113b0ace39510bc29f9af3e406f18d1069a
Qodirbergan <qodirberganqodirov9262@gmail.com>
2023-03-15 17:56:27 +0500

    aaa

---
+++ src/pages/Contacts.js
@@ -0,0 +1,7 @@
+function Contacts() {
+    return(
+        <h1>bu contacts</h1>
+    )
+
+}
+export default Contacts;
---
+++ src/pages/Error.js
@@ -0,0 +1,8 @@
+
+function Error() {
+    return(
+        <h1>Page not fount 404 error</h1>
+    )
+
+}
+export default Error;
